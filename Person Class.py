1f04994e7d2eeec7f600b00571e3c0df605f0514
Can Aydogan <can.aydogan.2004gmail.com>
2024-06-19 12:22:59 +0200

    Library class main building welcome messages added person class committed

---
+++ Person Class.py
@@ -0,0 +1,23 @@
+class person:
+    def __init__(self,name,id):
+        self.name=name
+        self.id=id
+
+    def __str__(self):
+        return f"Name {self.name},id {self.id}"
+
+class patron(person):
+    def __init__(self, name, patron_id):
+        super().__init__(name,patron_id)
+        self.borrowed_books= []
+
+    def borrow_book(self,book):
+        if book.available:
+            book.available=False
+            self.borrowed_books.append(book)
+
+
+    def return_book(self,book):
+        if book.available :
+            book.available=True
+            self.borrowed_books.remove(book)
