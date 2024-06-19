66d6decd814bd5327c4adbddaae054c90349ec23
CanAydogan <140336507+genaralgrievous@users.noreply.github.com>
2024-06-19 15:00:46 +0200

    Update Book.py

--- Book.py
+++ Book.py
@@ -12,30 +12,3 @@ class Book:
         return f"Name:{self.name} author:{self.author} isbn:{self.isbn}"
 
 
-class Library:
-    def __init__(self,name):
-        self.name = name
-        books = ["karamazov brothers","peter pan","harry potter"]
-        patrons = []
-
-    def add_books(self,book):
-        self.books.append(book)
-
-    def remove_books(self,book):
-        if book in self.books:
-            self.books.remove(book)
-
-    def add_patrons(self):
-        self.patrons.append(Patron)
-
-    def remove_patrons(self):
-        if Patron in self.patrons:
-            self.patrons.remove(Patron)
-
-    def list_books(self):
-         for book in self.books:
-             print(book)
-
-    def list_patrons(self):
-        for patron in self.patrons:
-            print(patron)
